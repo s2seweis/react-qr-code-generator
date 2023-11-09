55a1bf0b8534591cc4628846314b6378d6149bac
Sebastian Hans Weissenborn <58604870+s2seweis@users.noreply.github.com>
2023-11-09 14:14:16 +0100

    add increase and decrease buttons for the qr-code

--- src/components/QrGenerator/QrGenerator.jsx
+++ src/components/QrGenerator/QrGenerator.jsx
@@ -6,53 +6,71 @@ export default function QrGenerator() {
     const [qrData, setQrData] = useState({
         surname: '',
         prename: '',
-        age: '',
-        address: '',
+        street: '',
+        region: '',
+        zipCode: '',
+        country: '',
     }); // State to store form data
+
     const [isGenerated, setIsGenerated] = useState(false); // State to track if QR code is generated
+
+    const [qrSize, setQrSize] = useState(150); // State to track QR code size
 
     const handleInputChange = (e) => {
         const { name, value } = e.target;
-        setQrData({ ...qrData, [name]: value }); // Update form data based on user input
+        setQrData((prevData) => ({
+            ...prevData,
+            [name]: value,
+        }));
     };
 
     const handleSubmit = (e) => {
         e.preventDefault();
-        setIsGenerated(true); // Set the flag to indicate the QR code should be generated
+        setIsGenerated(true);
+    };
+
+    // const handleSizeChange = (value) => {
+    //     setQrSize((prevSize) => Math.max(50, prevSize + value));
+    // };
+
+    const handleSizeChange = (value) => {
+        const screenWidth = window.innerWidth;
+        const newQrSize = qrSize + value;
+
+        if (newQrSize >= 50 && newQrSize <= screenWidth - 100) {
+            setQrSize(newQrSize);
+        }
     };
 
     return (
-        <div>
+        <div className='head-form-container'>
             <div className='form-container'>
                 <h1>QrGenerator</h1>
                 <a href="https://www.npmjs.com/package/react-qr-code" target="_blank" rel="noopener noreferrer">
                     react-qr-code Documentation
                 </a>
                 <form style={{ marginTop: '20px' }} onSubmit={handleSubmit}>
-                    <label>
-                        Surname:
-                        <input type="text" name="surname" value={qrData.surname} onChange={handleInputChange} />
-                    </label>
-                    <label>
-                        Prename:
-                        <input type="text" name="prename" value={qrData.prename} onChange={handleInputChange} />
-                    </label>
-                    <label>
-                        Age:
-                        <input type="text" name="age" value={qrData.age} onChange={handleInputChange} />
-                    </label>
-                    <label>
-                        Address:
-                        <input type="text" name="address" value={qrData.address} onChange={handleInputChange} />
-                    </label>
+                    {Object.keys(qrData).map((key) => (
+                        <label key={key}>
+                            {key.charAt(0).toUpperCase() + key.slice(1)}:
+                            <input type="text" name={key} value={qrData[key]} onChange={handleInputChange} />
+                        </label>
+                    ))}
                     <button className='qr-button' type="submit">Generate QR Code</button>
                 </form>
+
             </div>
             {isGenerated && (
-                <div className='app-container' style={{ height: 'auto', margin: '20px auto', maxWidth: 150, width: '100%' }}>
+                <div className='app-container' style={{ height: 'auto', margin: '20px auto', width: '100%' }}>
+                    <h2 style={{ margin: '10px 0' }}>QR Code</h2>
+                    <div className="size-controls">
+                        <button className='button-qr' onClick={() => handleSizeChange(-10)}>-</button>
+                        <button className='button-qr' onClick={() => handleSizeChange(10)}>+</button>
+                    </div>
                     <QRCode
-                        value={JSON.stringify(qrData)} // Use qrData as the QR code value after converting it to a JSON string
-                        style={{ height: 'auto', maxWidth: '100%', width: '100%' }}
+                        value={JSON.stringify(qrData)}
+                        size={qrSize}
+                        style={{ height: 'auto', maxWidth: '100%', minwidth: '50%' }}
                         viewBox={'0 0 556 256'}
                     />
                 </div>
