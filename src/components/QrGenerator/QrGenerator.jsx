72b03cf28cfea104778fb6592a931472f367caa8
Sebastian Hans Weissenborn <58604870+s2seweis@users.noreply.github.com>
2023-11-09 13:25:34 +0100

    add some styles

--- src/components/QrGenerator/QrGenerator.jsx
+++ src/components/QrGenerator/QrGenerator.jsx
@@ -23,35 +23,37 @@ export default function QrGenerator() {
 
     return (
         <div>
-            <h1>QrGenerator</h1>
-            <a href="https://www.npmjs.com/package/react-qr-code" target="_blank" rel="noopener noreferrer">
-                react-qr-code Documentation
-            </a>
-            <form style={{ marginTop: "20px" }} onSubmit={handleSubmit}>
-                <label>
-                    Surname:
-                    <input type="text" name="surname" value={qrData.surname} onChange={handleInputChange} />
-                </label>
-                <label>
-                    Prename:
-                    <input type="text" name="prename" value={qrData.prename} onChange={handleInputChange} />
-                </label>
-                <label>
-                    Age:
-                    <input type="text" name="age" value={qrData.age} onChange={handleInputChange} />
-                </label>
-                <label>
-                    Address:
-                    <input type="text" name="address" value={qrData.address} onChange={handleInputChange} />
-                </label>
-                <button type="submit">Generate QR Code</button>
-            </form>
+            <div className='form-container'>
+                <h1>QrGenerator</h1>
+                <a href="https://www.npmjs.com/package/react-qr-code" target="_blank" rel="noopener noreferrer">
+                    react-qr-code Documentation
+                </a>
+                <form style={{ marginTop: '20px' }} onSubmit={handleSubmit}>
+                    <label>
+                        Surname:
+                        <input type="text" name="surname" value={qrData.surname} onChange={handleInputChange} />
+                    </label>
+                    <label>
+                        Prename:
+                        <input type="text" name="prename" value={qrData.prename} onChange={handleInputChange} />
+                    </label>
+                    <label>
+                        Age:
+                        <input type="text" name="age" value={qrData.age} onChange={handleInputChange} />
+                    </label>
+                    <label>
+                        Address:
+                        <input type="text" name="address" value={qrData.address} onChange={handleInputChange} />
+                    </label>
+                    <button className='qr-button' type="submit">Generate QR Code</button>
+                </form>
+            </div>
             {isGenerated && (
                 <div className='app-container' style={{ height: 'auto', margin: '20px auto', maxWidth: 150, width: '100%' }}>
                     <QRCode
                         value={JSON.stringify(qrData)} // Use qrData as the QR code value after converting it to a JSON string
                         style={{ height: 'auto', maxWidth: '100%', width: '100%' }}
-                        viewBox={`0 0 556 256`}
+                        viewBox={'0 0 556 256'}
                     />
                 </div>
             )}
